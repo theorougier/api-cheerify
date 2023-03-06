2d6aec9f3e5b0023edeff49d26f2eab4aa8932f9
TheoRougier-enoki <67681656+TheoRougier-enoki@users.noreply.github.com>
2023-03-06 12:36:58 +0100

    add key

--- config/server.js
+++ config/server.js
@@ -1,10 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'db4cbfe67eff033d7237556b65e1107e'),
+    },
   },
 });
