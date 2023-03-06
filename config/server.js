030c19a05d35a0013bb9369bee85d373f0864f93
TheoRougier-enoki <67681656+TheoRougier-enoki@users.noreply.github.com>
2023-03-06 19:59:14 +0100

    add admin

--- config/server.js
+++ config/server.js
@@ -1,9 +1,7 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'db4cbfe67eff033d7237556b65e1107e'),
-    },
+  proxy: true,
+  url: env('MY_HEROKU_URL'), // Sets the public URL of the application.
+  app: { 
+    keys: env.array('APP_KEYS')
   },
 });
